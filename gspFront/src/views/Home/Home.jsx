f498ba2e0896e4d322d843cf48d81262117c85d5
Almartinl <almartin.tpv@gmail.com>
2023-02-06 20:39:21 +0100

    add laptop

--- gspFront/src/views/Home/Home.jsx
+++ gspFront/src/views/Home/Home.jsx
@@ -10,8 +10,27 @@ export default function Home() {
         marginTop: "24px",
       }}
     >
-      <Grid>
-        <CarouselPrincipal />
+      <CarouselPrincipal />
+
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <img src="../../16box.JPG" width="100%" alt="img" />
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h2">Diseña tu bungalow</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <img src="../../22.jpg" width="100%" alt="img" />
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h2">Productos</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <img src="../../exp1.jpg" width="100%" alt="img" />
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h2">Nuestros trabajos</Typography>
+        </Grid>
       </Grid>
     </Container>
   );
