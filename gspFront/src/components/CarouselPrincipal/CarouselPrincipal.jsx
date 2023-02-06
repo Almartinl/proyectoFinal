bc352ca20afd4e3de98906e76dd4d2d107bc7b2e
Almartinl <almarti.tpv@gmail.com>
2023-02-06 13:30:22 +0100

    add pc

---
+++ gspFront/src/components/CarouselPrincipal/CarouselPrincipal.jsx
@@ -0,0 +1,108 @@
+import { Button } from "@mui/material";
+
+export default function CarouselPrincipal() {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide"
+      data-bs-ride="false"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="../../public/22.jpg"
+            className="d-block w-100"
+            alt="carrusel"
+          />
+          <div className="carousel-caption carousel-text">
+            <h3 className="shadow-text display-2 fw-bold">BIENVENIDO</h3>
+            <h4 className="shadow-text fw-bold">tu bungalow a medida</h4>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ backgroundColor: "green" }}
+            >
+              Ver
+            </Button>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="../../public/44.jpg"
+            className="d-block w-100 "
+            alt="carrusel"
+          />
+          <div className="carousel-caption carousel-text">
+            <h3 className="shadow-text display-2 fw-bold">BIENVENIDO</h3>
+            <h4 className="shadow-text fw-bold">tu bungalow a medida</h4>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ backgroundColor: "green" }}
+            >
+              Ver
+            </Button>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="../../public/16box.jpg"
+            className="d-block w-100"
+            alt="carrusel"
+          />
+          <div className="carousel-caption carousel-text">
+            <h3 className="shadow-text display-2 fw-bold">BIENVENIDO</h3>
+            <h4 className="shadow-text fw-bold">tu bungalow a medida</h4>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ backgroundColor: "green" }}
+            >
+              Ver
+            </Button>
+          </div>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
--- gspFront/src/views/Home/Home.jsx
+++ gspFront/src/views/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./home.css";
 import { Button, Container, Grid, Typography } from "@mui/material";
+import CarouselPrincipal from "../../components/CarouselPrincipal/CarouselPrincipal";
 
 export default function Home() {
   return (
@@ -7,110 +8,10 @@ export default function Home() {
       maxWidth="xl"
       sx={{
         marginTop: "24px",
-        position: "relative",
       }}
     >
       <Grid>
-        <div
-          id="carouselExampleCaptions"
-          className="carousel slide"
-          data-bs-ride="false"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                src="../../public/22.jpg"
-                className="d-block w-100"
-                alt="carrusel"
-              />
-              <div className="carousel-caption ">
-                <h2 className="display-2">ofertas de otoño</h2>
-                <p>
-                  Some representative placeholder content for the first slide.
-                </p>
-                <Button
-                  variant="contained"
-                  color="success"
-                  sx={{ backgroundColor: "green" }}
-                >
-                  Ver
-                </Button>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                src="../../public/44.jpg"
-                className="d-block w-100 "
-                alt="carrusel"
-              />
-              <div className="carousel-caption">
-                <h5>Second slide label</h5>
-                <p>
-                  Some representative placeholder content for the second slide.
-                </p>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img
-                src="../../public/22.jpg"
-                className="d-block w-100"
-                alt="carrusel"
-              />
-              <div className="carousel-caption">
-                <h5>Third slide label</h5>
-                <p>
-                  Some representative placeholder content for the third slide.
-                </p>
-              </div>
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
+        <CarouselPrincipal />
       </Grid>
     </Container>
   );
